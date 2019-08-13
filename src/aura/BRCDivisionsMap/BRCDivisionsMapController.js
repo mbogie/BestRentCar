1b3db316865c57980d07096a40b24176a4bbaa69
Bogusz <bogusz.m85@gmail.com>
2019-08-13 23:26:37 +0200

    smallChangeInMapDisplay

--- src/aura/BRCDivisionsMap/BRCDivisionsMapController.js
+++ src/aura/BRCDivisionsMap/BRCDivisionsMapController.js
@@ -4,7 +4,7 @@
         var markers = event.getParam("markers");
         component.set('v.mapMarkers',markers);
         if(markers.length == 1){
-           component.set('v.zoomLevel', 8);
+           component.set('v.zoomLevel', 13);
         } else if(markers.length > 5){
            component.set('v.zoomLevel', 3);
         }else{
