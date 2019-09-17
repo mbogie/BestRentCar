108933e3a8dd59bfb3e73097f18e7cc3a68d635b
Bogusz <bogusz.m85@gmail.com>
2019-09-17 15:33:58 +0200

    addDaysNumber

--- src/aura/BRCProductPrice/BRCProductPriceController.js
+++ src/aura/BRCProductPrice/BRCProductPriceController.js
@@ -7,8 +7,9 @@
         let recordItem = component.get("v.productItem");
         let priceList = component.get("v.priceList");
         let objectItem = {
-            price: priceList,
-           product: recordItem
+            price: priceList[0],
+            product: recordItem,
+            days: component.get("v.daysNumber")
         };
         let userId = $A.get("$SObjectType.CurrentUser.Id");
         let cartJson = sessionStorage.getItem('cart'+userId);
@@ -16,16 +17,25 @@
             console.log('add to list');
             let basketList = JSON.parse(cartJson);
             basketList.push(objectItem);
-            console.log(basketList);
+           // console.log(basketList);
             sessionStorage.setItem('cart'+userId, JSON.stringify(basketList));
             component.find("toastCmp").showToastModel($A.get("{!$Label.c.BRC_Product_Added_to_Cart}"), "info");
         } else {
             console.log('new list2');
             let basketList = [objectItem];
-            console.log(basketList);
+          //  console.log(basketList);
             sessionStorage.setItem('cart'+userId, JSON.stringify(basketList));
             component.find("toastCmp").showToastModel($A.get("{!$Label.c.BRC_Product_Added_to_Cart}"), "info");
         }
     },
 
+    plusDay : function(component, event,helper) {
+        let days = component.get("v.daysNumber");
+        component.set("v.daysNumber", Number(days)+1);
+    },
+
+    minusDay : function(component, event,helper) {
+        let days = component.get("v.daysNumber");
+        component.set("v.daysNumber", Number(days)-1);
+    },
 })
